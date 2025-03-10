070e72ff461cb27e9b03a1367eddb1a8aae0e7e3
Aaryan Agnihotri <agnihotri.aaryan@etoncollege.org.uk>
2025-03-10 23:00:02 +0000

    embedded youtube video

--- Music_Player/ContentView.swift
+++ Music_Player/ContentView.swift
@@ -6,14 +6,51 @@
 //
 
 import SwiftUI
+import AVKit
+import WebKit
+
+struct WebView: UIViewControllerRepresentable {
+    var url: URL
+    
+    func makeUIViewController(context: Context) -> ViewController {
+        let viewController = ViewController()
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+        let request = URLRequest(url: url)
+        uiViewController.webView.load(request)
+    }
+}
+
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"https://www.youtube.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+}
 
 struct ContentView: View {
+    @State var youtubeURL = URL(string: "https://www.youtube.com/watch?v=1JGNwfii30s")!
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            WebView(url: youtubeURL)
+                            .frame(width: 320, height: 240)
         }
         .padding()
     }
